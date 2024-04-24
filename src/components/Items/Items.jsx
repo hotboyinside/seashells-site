061b765b7f9e05c94da3aabacf13f1a73b7e6e58
hotboyinside <rusov_pavel2002@mail.ru>
2024-04-24 17:10:16 +0300

    half work of media css

--- src/components/Items/Items.jsx
+++ src/components/Items/Items.jsx
@@ -5,7 +5,7 @@ export default function Items() {
     return (
         <section className='items'>
             <div className="items__container">
-                <h2 className='items__title'>ЭКСПОНАТЫ</h2>
+                <h2 className='items__title'>экспонаты</h2>
                 <Slider />
             </div>
         </section>
