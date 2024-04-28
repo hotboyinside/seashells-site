a008b52d33cd91893caf7d9299ca4a3979edc07f
hotboyinside <rusov_pavel2002@mail.ru>
2024-04-28 15:16:44 +0300

    started 480px media css

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,22 @@
 import './Hero.css'
+import Logo from '../Logo/Logo'
+import Button from '../Button/Button'
 
 export default function Hero() {
     return (
         <section className="hero">
             <div className="hero__container">
-                <div></div>
+                <div className='hero__blank-block'></div>
                 <div className="hero__content">
+                    <div className='hero__header-mobile'>
+                        <Logo />
+                        <Button>Стань участником</Button>
+                    </div>
                     <h1 className="title hero_title">Выставка-продажа морских раковин</h1>
                     <address className="hero_address">Санкт-Петербург, Невский проспект, д. 35</address>
                     <p className="hero__date">10 июля 2024, 10:00</p>
                 </div>
-                <div></div>
+                <div className='hero__blank-block'></div>
             </div>
         </section>
     )
