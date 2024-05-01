1a825ca03643b60529881087f880795508ec6caf
hotboyinside <rusov_pavel2002@mail.ru>
2024-05-01 23:40:01 +0300

    form data send to backend in db

--- src/components/Participant/Participant.jsx
+++ src/components/Participant/Participant.jsx
@@ -1,6 +1,29 @@
+import { useState } from 'react'
+
 import './Participant.css'
+import api from '../../api';
 
 export default function Participant() {
+    const [formData, setFormData] = useState({
+        name: "",
+        phone_number: "",
+        description: ""
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        console.log(value)
+        setFormData((prevState) => ({...prevState, [name]: value}));
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault()
+        const {status} = await api.post('participants/', formData)
+        if (status == 201) {
+            alert('Заявка успешно отправлена!')
+        }
+    }
+
     function handleMouseOver() {
         const leftTicketBlock = document.querySelector('.form__left')
         leftTicketBlock.classList.add('active-btn')
@@ -16,11 +39,11 @@ export default function Participant() {
                 <h2 className='participant__title title'>Стань участником</h2>
                 <p className="participant__desc">Если вы&nbsp;коллекционер морских раковин и&nbsp;желаете показать и&nbsp;продать свои раковины, заполните форму ниже.</p>
                 <div className="participant__ticket">
-                    <form className='participant__form form' action="" method='POST'>
+                    <form onSubmit={handleFormSubmit} className='participant__form form' action="http://127.0.0.1:8000/participants" method='POST'>
                         <div className="form__left">
-                            <input className='form__input' type="text" name="user_name" placeholder='Ваше имя' id="" required />
-                            <input className='form__input' type="text" name="user_phone" placeholder='Телефон' id="" required />
-                            <input className='form__input' type="text" name="user_desc" placeholder='Расскажите, что вас интересует' id="" required />
+                            <input onChange={handleChange} className='form__input' type="text" name="name" value={formData.name} placeholder='Ваше имя' required />
+                            <input onChange={handleChange} className='form__input' type="tel" name="phone_number" value={formData.phone_number} placeholder='Телефон' title='Телефон должен быть формата "+7"' pattern='^\+\d{11}$' required />
+                            <input onChange={handleChange} className='form__input' type="text" name="description" value={formData.description} placeholder='Расскажите, что вас интересует' required />
                         </div>
                         <button onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className='btn-reset form__btn'>ОТПРАВИТЬ</button>
                     </form>
