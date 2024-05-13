20af130e36c9ce8cc5d249f70ea132ec3dd5799b
hotboyinside <rusov_pavel2002@mail.ru>
2024-05-13 13:12:35 +0300

    fixed

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,10 @@ const root = resolve(__dirname, 'src')
 const outDir = resolve(__dirname, 'dist')
 
 export default defineConfig({
+  root,
   plugins: [react()],
   build: {
+    outDir,
     rollupOptions: {
       main: resolve(root, 'pages', 'Home', 'index.html'),
       afisha: resolve(root, 'pages', 'Afisha', 'index.html')
