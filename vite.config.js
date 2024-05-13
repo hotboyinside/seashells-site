b25a81572c722514a58cb1072595369fb2318320
hotboyinside <rusov_pavel2002@mail.ru>
2024-05-13 13:05:15 +0300

    modified config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      main: resolve(root, 'pages', 'Home', 'index.html'),
+      afisha: resolve(root, 'pages', 'Afisha', 'index.html')
+    }
+  }
 })
